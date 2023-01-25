01d748eac2352a27921e3b36414838f7cae0d341
Eliel Lopes Dos Santos <ellsantos@rd.com.br>
2023-01-25 15:59:54 -0300

    alterando retorno inicial para 151 pokemons

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -9,7 +9,7 @@ import { map, Observable, tap } from 'rxjs';
 })
 export class PokeApiService {
 
-  private url: string = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100";
+  private url: string = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151";
 
   constructor(
     private http: HttpClient
